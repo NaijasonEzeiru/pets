61d2b1f8b88168663bdba42e6350fa52fb320e52
NaijasonEzeiru <67031753+NaijasonEzeiru@users.noreply.github.com>
2023-11-02 11:20:37 +0100

    fixed error of fetch being called inside of the component

--- src/components/petsCard.tsx/HomePetsCard.tsx
+++ src/components/petsCard.tsx/HomePetsCard.tsx
@@ -2,12 +2,16 @@ import { PetSchemaType } from '@/utils/schemas';
 import VerticalProductCard from './VerticalPetsCard';
 import { apiAddress } from '@/utils/variables';
 
-const HomePetsCard = async () => {
-  const data = await fetch(`${apiAddress}/api/pets`, {
+async function fetchPets() {
+  const res = await fetch(`${apiAddress}/api/pets`, {
     cache: 'no-store'
   });
-  const pets: PetSchemaType[] = await data.json();
-  console.log(pets);
+  if (!res.ok) return undefined;
+  return res.json();
+}
+
+const HomePetsCard = async () => {
+  const pets: PetSchemaType[] = await fetchPets();
 
   return (
     <div className='pl-3 md:pl-6 w-full py-11 grid gap-x-3 md:gap-x-5 gap-y-4 md:gap-y-6 gtc grid-flow-row'>
