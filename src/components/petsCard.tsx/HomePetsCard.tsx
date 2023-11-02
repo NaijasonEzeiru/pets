2ec26ce3c3dd5c876e1d3e7d3013af89f8632c24
NaijasonEzeiru <67031753+NaijasonEzeiru@users.noreply.github.com>
2023-11-02 11:12:58 +0100

    .

--- src/components/petsCard.tsx/HomePetsCard.tsx
+++ src/components/petsCard.tsx/HomePetsCard.tsx
@@ -11,17 +11,18 @@ const HomePetsCard = async () => {
 
   return (
     <div className='pl-3 md:pl-6 w-full py-11 grid gap-x-3 md:gap-x-5 gap-y-4 md:gap-y-6 gtc grid-flow-row'>
-      {pets.map((v) => (
-        <VerticalProductCard
-          key={v.id}
-          id={v.id!}
-          location={`${v.city}, ${v.state}`}
-          productName={`${v.breed} ${
-            v.purebred == 'Yes' ? 'purebred' : 'mixed'
-          } (${v.age}) ${v.gender}`}
-          img={v.imgs[0]}
-        />
-      ))}
+      {pets.length > 0 &&
+        pets.map((v) => (
+          <VerticalProductCard
+            key={v.id}
+            id={v.id!}
+            location={`${v.city}, ${v.state}`}
+            productName={`${v.breed} ${
+              v.purebred == 'Yes' ? 'purebred' : 'mixed'
+            } (${v.age}) ${v.gender}`}
+            img={v.imgs[0]}
+          />
+        ))}
     </div>
   );
 };
